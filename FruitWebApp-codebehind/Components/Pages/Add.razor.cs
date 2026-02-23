16b5c7b620eb7d0eeca0bf08e65d39e1646848b3
LuskGPM <145815917+LuskGPM@users.noreply.github.com>
2026-02-23 20:53:20 -0300

    consumindo api

---
+++ FruitWebApp-codebehind/Components/Pages/Add.razor.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using FruitWebApp.Models;
+using System.Text.Json;
+using System.Text;
+
+namespace FruitWebApp.Components.Pages;
+
+public partial class Add : ComponentBase
+{
+    // IHttpClientFactory set using dependency injection 
+    [Inject]
+    public required IHttpClientFactory HttpClientFactory { get; set; }
+
+    // NavigationManager set using dependency injection
+    [Inject]
+    private NavigationManager? NavigationManager { get; set; }
+
+    // Add the data model and bind the form data to it
+    [SupplyParameterFromForm]
+    private FruitModel? _fruitList { get; set; }
+
+    protected override void OnInitialized() => _fruitList ??= new();
+
+    // Begin POST operation code
+    private async Task Submit()
+    {
+        var jsonContent = new StringContent(JsonSerializer.Serialize(_fruitList),
+        Encoding.UTF8,
+        "application/json");
+
+        var httpClient = HttpClientFactory.CreateClient("FruitAPI");
+
+        using HttpResponseMessage response = await httpClient.PostAsync("/fruits", jsonContent);
+
+        if (response.IsSuccessStatusCode)
+        {
+            NavigationManager?.NavigateTo("/");
+        }
+    }
+    // End POST operation code
+}
